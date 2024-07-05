66850e23f3c84ca15c9205d1f9095fa2c184151a
mirnal <mirnalkishor106@gmail.com>
2024-07-05 23:38:57 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -14,12 +14,11 @@ function generateReadme() {
     const built = document.getElementById('built').value;
     const freeTime = document.getElementById('freeTime').value;
     const contact = document.getElementById('contact').value;
-    const favorites = document.getElementById('favorites').value;
 
     const readmeContent = `
 # Hi 👋, I'm ${name}
 
-# ${Profile}
+${Profile}
 
 ## Connect with me:
 <p>
@@ -27,7 +26,7 @@ function generateReadme() {
    <a href="${github}" target="_blank"><img align="center" src="https://github.com/Mirnalkishor/GitHub-Profile-README-Generator/blob/main/icons/github.png" alt="GitHub" height="30" width="40" /></a>
    <a href="${geeksforgeeks}" target="_blank"><img align="center" src="https://github.com/Mirnalkishor/GitHub-Profile-README-Generator/blob/main/icons/geeks-for-geeks.png" alt="GeeksforGeeks" height="30" width="40" /></a>
    <a href="${hackerrank}" target="_blank"><img align="center" src="https://github.com/Mirnalkishor/GitHub-Profile-README-Generator/blob/main/icons/hackerrank.png" alt="Hackerrank" height="30" width="40" /></a>
-   <a href="${leetcode}" target="_blank"><img align="center" src="icons/leetcode.png" alt="LeetCode" height="30" width="40" /></a>
+   <a href="${leetcode}" target="_blank"><img align="center" src="https://github.com/Mirnalkishor/GitHub-Profile-README-Generator/blob/main/icons/leet-code.png" height="30" width="40" /></a>
    <a href="${instagram}" target="_blank"><img align="center" src="https://github.com/Mirnalkishor/GitHub-Profile-README-Generator/blob/main/icons/instagram.png" alt="Instagram" height="30" width="40" /></a>
 </p>
 
@@ -38,9 +37,6 @@ function generateReadme() {
 - 👨🏻‍💻 &nbsp; Most of my projects are available on: [${projectName}](${projectLink})
 - 👨‍💻 &nbsp; Free time activities: ${freeTime}
 - 📫 &nbsp; How to reach me: ${contact}
-
-## My Absolute Favorites:
-${favorites}
 `;
 
     document.getElementById('generatedReadme').value = readmeContent.trim();
