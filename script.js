1a7f65ab665c93ec3f2b9f3574456b8a4034e8ff
mirnal <mirnalkishor106@gmail.com>
2024-07-05 18:15:22 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+function generateReadme() {
+    const name = document.getElementById('name').value;
+    const currentlyWorking = document.getElementById('currentlyWorking').value;
+    const projectName = document.getElementById('projectName').value;
+    const projectLink = document.getElementById('projectLink').value;
+    const linkedin = document.getElementById('linkedin').value;
+    const github = document.getElementById('github').value;
+    const instagram = document.getElementById('instagram').value;
+    const hackerrank = document.getElementById('hackerrank').value;
+    const leetcode = document.getElementById('leetcode').value;
+    const geeksforgeeks = document.getElementById('geeksforgeeks').value;
+    const currentlyLearning = document.getElementById('currentlyLearning').value;
+    const built = document.getElementById('built').value;
+    const freeTime = document.getElementById('freeTime').value;
+    const contact = document.getElementById('contact').value;
+    const favorites = document.getElementById('favorites').value;
+
+    const readmeContent = `
+# Hi 👋, I'm ${name}
+
+## Connect with me:
+<p>
+   <a href="${linkedin}" target="_blank"><img align="center" src="icons/linkedin.png" alt="LinkedIn" height="30" width="40" /></a>
+   <a href="${github}" target="_blank"><img align="center" src="icons/github.png" alt="GitHub" height="30" width="40" /></a>
+   <a href="${geeksforgeeks}" target="_blank"><img align="center" src="icons/geeksforgeeks.png" alt="GeeksforGeeks" height="30" width="40" /></a>
+   <a href="${hackerrank}" target="_blank"><img align="center" src="icons/hackerrank.png" alt="Hackerrank" height="30" width="40" /></a>
+   <a href="${leetcode}" target="_blank"><img align="center" src="icons/leetcode.png" alt="LeetCode" height="30" width="40" /></a>
+   <a href="${instagram}" target="_blank"><img align="center" src="icons/instagram.png" alt="Instagram" height="30" width="40" /></a>
+</p>
+
+## Talking about Personal Stuffs:
+- 🔭 I’m currently working on: ${currentlyWorking}
+- 🏗 &nbsp; Building: ${built}
+- 🚀 &nbsp; I’m currently learning: ${currentlyLearning}
+- 👨🏻‍💻 &nbsp; Most of my projects are available on: [${projectName}](${projectLink})
+- 👨‍💻 &nbsp; Free time activities: ${freeTime}
+- 📫 &nbsp; How to reach me: ${contact}
+
+## My Absolute Favorites:
+${favorites}
+`;
+
+    document.getElementById('generatedReadme').value = readmeContent.trim();
+}
+
+function copyReadme() {
+    const readmeContent = document.getElementById('generatedReadme');
+    readmeContent.select();
+    document.execCommand('copy');
+    alert('README content copied to clipboard!');
+}
