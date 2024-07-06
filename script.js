0be625374d35c66eaa853712dda48ed321059af3
mirnal <mirnalkishor106@gmail.com>
2024-07-06 13:33:53 +0530

    Major Updates

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function generateReadme() {
     const name = document.getElementById('name').value;
-    const Profile = document.getElementById('Profile').value;
+    const profile = document.getElementById('profile').value;
     const currentlyWorking = document.getElementById('currentlyWorking').value;
     const projectName = document.getElementById('projectName').value;
     const projectLink = document.getElementById('projectLink').value;
@@ -14,23 +14,24 @@ function generateReadme() {
     const built = document.getElementById('built').value;
     const freeTime = document.getElementById('freeTime').value;
     const contact = document.getElementById('contact').value;
-
     const readmeContent = `
-# Hi 👋, I'm ${name}
 
-${Profile}
+<p align="center">
+<img src="https://readme-typing-svg.herokuapp.com?color=E22FE4&width=380&height=28&lines=Hi👋+I'm+${name}..;Open-Source+Enthusiast..;Learning+In+Public..;Nice+To+Meet+You+....&center=true"></a></p>
+
+${profile}
 
 ## Connect with me:
 <p>
-   <a href="${linkedin}" target="_blank"><img align="center" src="https://github.com/Mirnalkishor/GitHub-Profile-README-Generator/blob/main/icons/linked-in.png" alt="LinkedIn" height="30" width="40" /></a>
-   <a href="${github}" target="_blank"><img align="center" src="https://github.com/Mirnalkishor/GitHub-Profile-README-Generator/blob/main/icons/github.png" alt="GitHub" height="30" width="40" /></a>
-   <a href="${geeksforgeeks}" target="_blank"><img align="center" src="https://github.com/Mirnalkishor/GitHub-Profile-README-Generator/blob/main/icons/geeks-for-geeks.png" alt="GeeksforGeeks" height="30" width="40" /></a>
-   <a href="${hackerrank}" target="_blank"><img align="center" src="https://github.com/Mirnalkishor/GitHub-Profile-README-Generator/blob/main/icons/hackerrank.png" alt="Hackerrank" height="30" width="40" /></a>
-   <a href="${leetcode}" target="_blank"><img align="center" src="https://github.com/Mirnalkishor/GitHub-Profile-README-Generator/blob/main/icons/leet-code.png" height="30" width="40" /></a>
-   <a href="${instagram}" target="_blank"><img align="center" src="https://github.com/Mirnalkishor/GitHub-Profile-README-Generator/blob/main/icons/instagram.png" alt="Instagram" height="30" width="40" /></a>
+<a href="${linkedin}" target="_blank"><img align="center" src="https://github.com/Mirnalkishor/GitHub-Profile-README-Generator/blob/main/icons/linked-in.png" alt="LinkedIn" height="30" width="40" /></a>
+<a href="${github}" target="_blank"><img align="center" src="https://github.com/Mirnalkishor/GitHub-Profile-README-Generator/blob/main/icons/github.png" alt="GitHub" height="30" width="40" /></a>
+<a href="${geeksforgeeks}" target="_blank"><img align="center" src="https://github.com/Mirnalkishor/GitHub-Profile-README-Generator/blob/main/icons/geeks-for-geeks.png" alt="GeeksforGeeks" height="30" width="40" /></a>
+<a href="${hackerrank}" target="_blank"><img align="center" src="https://github.com/Mirnalkishor/GitHub-Profile-README-Generator/blob/main/icons/hackerrank.png" alt="Hackerrank" height="30" width="40" /></a>
+<a href="${leetcode}" target="_blank"><img align="center" src="https://github.com/Mirnalkishor/GitHub-Profile-README-Generator/blob/main/icons/leet-code.png" height="30" width="40" /></a>
+<a href="${instagram}" target="_blank"><img align="center" src="https://github.com/Mirnalkishor/GitHub-Profile-README-Generator/blob/main/icons/instagram.png" alt="Instagram" height="30" width="40" /></a>
 </p>
 
-## 🗣 Talking about Personal Stuffs:
+## 🗣 Talking about Personal Stuffs:  <img align=right src="https://github.com/7oSkaaa/7oSkaaa/blob/main/Images/about_me.gif?raw=true" alt="About Me GIF" width="180px"><br/>
 - 🔭 I’m currently working on: ${currentlyWorking}
 - 🏗 &nbsp; Building: ${built}
 - 🚀 &nbsp; I’m currently learning: ${currentlyLearning}
@@ -38,7 +39,13 @@ ${Profile}
 - 👨‍💻 &nbsp; Free time activities: ${freeTime}
 - 📫 &nbsp; How to reach me: ${contact}
 
-Thank you for reading, Share your ❤️ And follow me for update on some of my more amazing projects🌟 
+## 📊 GitHub Stats:
+![](https://github-readme-stats.vercel.app/api/top-langs/?username=${name}&border=false&include_all_commits=true&count_private=true&layout=compact)
+![](https://github-readme-stats.vercel.app/api?username=${name}&_border=false&include_all_commits=true&count_private=true)<br/>
+![](https://github-readme-streak-stats.herokuapp.com/?user=${name}&hide_border=false)
+
+<p align=center>
+Thank you for reading, Share your ❤️ And follow me for update🌟</p> 
 `;
 
     document.getElementById('generatedReadme').value = readmeContent.trim();
